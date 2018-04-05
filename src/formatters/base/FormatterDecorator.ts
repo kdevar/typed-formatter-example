bc569c3ab1b527e0265c7dda70042a5dfd58ed1c
kdevarakonda <kdevarakonda@costar.com>
2018-04-05 14:31:48 -0400

    module private

--- src/formatters/base/FormatterDecorator.ts
+++ src/formatters/base/FormatterDecorator.ts
@@ -1,24 +1,20 @@
 import {IFormatters} from '../contract';
 import {country} from './BaseFormatter';
 
-export const formatterNameMetadataKeyName = "formatterName";
-export const formatterNameMetadataCountryName = "formatterCountry";
-
+const formatterNameMetadataKeyName = "formatterName";
+const formatterNameMetadataCountryName = "formatterCountry";
 export function formatter<K extends keyof IFormatters>(name:K, country:country){    
     return target => {
         Reflect.defineMetadata(formatterNameMetadataKeyName, name, target);
         Reflect.defineMetadata(formatterNameMetadataCountryName, country, target);
     }    
 }
-
 export function isTargetFormatter(target, interfce, country){    
     return interfce === getFormatterInterface(target) && country === getFormatterCountry(target); 
 }
-
 export function getFormatterInterface(target){
     return Reflect.getMetadata(formatterNameMetadataKeyName, target);
 }
-
 export function getFormatterCountry(target){
     return Reflect.getMetadata(formatterNameMetadataCountryName, target);
 }
